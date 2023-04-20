94e485b359ae5e03b8ec58ce3ab4b69694c7305d
Divyue30597 <tomchi.sharma305@outlook.com>
2023-04-20 20:13:45 +0530

    completed 'complete credit card detail saved'

--- src/Component/FormSubmitSuccess/FormSubmitSuccess.jsx
+++ src/Component/FormSubmitSuccess/FormSubmitSuccess.jsx
@@ -1,7 +1,17 @@
+import { ImageComp } from "../ImageComp/ImageComp";
+import "./formsubmitsuccess.css";
+import completedSavingCardDetailsImage from "../../assets/icon-complete.svg";
+
 export function FormSubmitSuccess() {
   return (
     <div className="form-submit-success">
-      <h1>Success</h1>
+      <ImageComp
+        altText={"Completed saving card details"}
+        imageName={completedSavingCardDetailsImage}
+      />
+      <h2>Thank You!</h2>
+      <p>We've added your card details</p>
+      <button>Continue</button>
     </div>
   );
 }
